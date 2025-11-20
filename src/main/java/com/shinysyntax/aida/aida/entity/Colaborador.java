700a399a914ae36caeeb4786ea86a3995421374d
Lana <al.t.loureiro0@gmail.com>
2025-11-20 20:53:15 -0300

    refactor: clean up Colaborador entity by removing unnecessary whitespace and ensuring default values are set in the constructor

--- src/main/java/com/shinysyntax/aida/aida/entity/Colaborador.java
+++ src/main/java/com/shinysyntax/aida/aida/entity/Colaborador.java
@@ -23,8 +23,6 @@ public class Colaborador {
     @Column(name = "cpf_colaborador")
     private Long cpf;
 
-    
-
     @Column(nullable = false)
     private String nome;
 
@@ -34,20 +32,25 @@ public class Colaborador {
     @Column
     private String telefone;
 
+    @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
 
+    @Column
     private String cargo;
+
     @Convert(converter = com.shinysyntax.aida.aida.converter.ModalidadeConverter.class)
     @Column(length = 20)
     private com.shinysyntax.aida.aida.enums.Modalidade modalidade;
+
+    @Column(name = "data_admissao")
     private LocalDate dataAdmissao;
 
     @Lob
-    @Column(columnDefinition = "CLOB DEFAULT 'NÃO POSSUI PROBLEMAS DE SAÚDE'")
+    @Column
     private String problemaSaude = "NÃO POSSUI PROBLEMAS DE SAÚDE";
 
     @Lob
-    @Column(columnDefinition = "CLOB DEFAULT 'NÃO FAZ USO DE MEDICAMENTO CONTROLADO'")
+    @Column
     private String medicamentoUsoDiario = "NÃO FAZ USO DE MEDICAMENTO CONTROLADO";
 
     @OneToMany(mappedBy = "colaborador", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -59,35 +62,45 @@ public class Colaborador {
     private List<RegistroDiario> registros = new ArrayList<>();
 
     public Colaborador() {
-        // ensure default values on entity level as well
+        // garante valores default mesmo que não setados
         if (this.problemaSaude == null) this.problemaSaude = "NÃO POSSUI PROBLEMAS DE SAÚDE";
         if (this.medicamentoUsoDiario == null) this.medicamentoUsoDiario = "NÃO FAZ USO DE MEDICAMENTO CONTROLADO";
     }
 
-    // getters and setters
+    // getters e setters
     public Long getCpf() { return cpf; }
     public void setCpf(Long cpf) { this.cpf = cpf; }
-    
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
     public String getTelefone() { return telefone; }
     public void setTelefone(String telefone) { this.telefone = telefone; }
+
     public LocalDate getDataNascimento() { return dataNascimento; }
     public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+
     public String getCargo() { return cargo; }
     public void setCargo(String cargo) { this.cargo = cargo; }
+
     public com.shinysyntax.aida.aida.enums.Modalidade getModalidade() { return modalidade; }
     public void setModalidade(com.shinysyntax.aida.aida.enums.Modalidade modalidade) { this.modalidade = modalidade; }
+
     public LocalDate getDataAdmissao() { return dataAdmissao; }
     public void setDataAdmissao(LocalDate dataAdmissao) { this.dataAdmissao = dataAdmissao; }
+
     public String getProblemaSaude() { return problemaSaude; }
     public void setProblemaSaude(String problemaSaude) { this.problemaSaude = problemaSaude; }
+
     public String getMedicamentoUsoDiario() { return medicamentoUsoDiario; }
     public void setMedicamentoUsoDiario(String medicamentoUsoDiario) { this.medicamentoUsoDiario = medicamentoUsoDiario; }
+
     public List<Agenda> getAgendas() { return agendas; }
     public void setAgendas(List<Agenda> agendas) { this.agendas = agendas; }
+
     public List<RegistroDiario> getRegistros() { return registros; }
     public void setRegistros(List<RegistroDiario> registros) { this.registros = registros; }
 }
